f158e27dbf8083d5b638dd303dd68c06964637b3
thegraniti <34227605+thegraniti@users.noreply.github.com>
2023-03-20 21:52:39 +0100

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -24,7 +24,7 @@ function CreatePost() {
     if(form.prompt && form.photo){
       setLoading(true);
       try{
-          const response = await fetch('https://dall-e-qttf.onrender.com/api/v1/post',
+          const response = await fetch('http://localhost:8080/api/v1/post',
           {
             method: 'POST',
             headers: {
@@ -49,7 +49,7 @@ function CreatePost() {
       if(form.prompt){
         try {
           setGenerateImg(true)          
-          const response = await fetch('https://dall-e-qttf.onrender.com/api/v1/dalle', {
+          const response = await fetch('http://localhost:8080/api/v1/dalle', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
